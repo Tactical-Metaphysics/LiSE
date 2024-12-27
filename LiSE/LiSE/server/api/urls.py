b8efa2fdf0a1672d37482d7187eb7ec89af379e8
Zachary Spector <public@zacharyspector.com>
2024-12-28 10:25:53 +1300

    Stub out `CharacterAPIView`

--- LiSE/LiSE/server/api/urls.py
+++ LiSE/LiSE/server/api/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = [path('api/hello', HellowAPIView.as_view(), name='hello_api_endpoint')]
+urlpatterns = [
+	path(
+		"api/character",
+		CharacterAPIView.as_view(),
+		name="character_api_endpoint",
+	)
+]
--- LiSE/LiSE/server/api/views.py
+++ LiSE/LiSE/server/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+
+class CharacterAPIView(APIView):
+	def get_renderer_context(self):
+		ret = super().get_renderer_context()
+		# add the engine to ret
+		return ret
