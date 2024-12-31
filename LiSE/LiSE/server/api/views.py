809b2a8b6bfae3ce9b22d2a5017123272066f3f9
Zachary Spector <public@zacharyspector.com>
2024-12-31 16:04:46 +1300

    Use `@api_view`

--- LiSE/LiSE/server/api/views.py
+++ LiSE/LiSE/server/api/views.py
@@ -1,49 +1,51 @@
 from django.contrib.auth.models import User
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
 from .models import Game
 from .serializers import UserSerializer, GameSerializer
 
 
 # Create your views here.
-@csrf_exempt
+@api_view(["GET", "POST"])
 def game_list(request):
 	"""List all games, or create a new game"""
 	if request.method == "GET":
 		games = Game.objects.all()
 		serializer = GameSerializer(games, many=True)
-		return JsonResponse(serializer.data, safe=False)
+		return Response(serializer.data)
 	elif request.method == "POST":
 		data = JSONParser().parse(request)
 		serializer = GameSerializer(data=data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(["GET", "PUT", "DELETE"])
 def game_detail(request, pk):
 	try:
 		game = Game.objects.get(pk=pk)
 	except Game.DoesNotExist:
-		return HttpResponse(status=404)
+		return Response(status=status.HTTP_404_NOT_FOUND)
 	if request.method == "GET":
 		serializer = GameSerializer(game)
-		return JsonResponse(serializer.data)
+		return Response(serializer.data)
 	elif request.method == "PUT":
 		data = JSONParser().parse(request)
 		serializer = GameSerializer(game, data=data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data)
-		return JsonResponse(serializer.errors, status=400)
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 	elif request.method == "DELETE":
 		game.delete()
-		return HttpResponse(status=204)
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CharacterAPIView(APIView):
